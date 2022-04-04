c36937f43e9d892c1f2c03552d0ee42e11cf1967
yuri Jung <101693599+Yuri-Jung@users.noreply.github.com>
2022-04-04 09:58:02 +0900

    Add files via upload

---
+++ booleanTest/src/booleanTest/Boolean.java
@@ -0,0 +1,13 @@
+package booleanTest;
+
+public class Boolean {
+	public static void main(String args[]) {
+		boolean b;
+		
+		b=true;
+		System.out.println("b : " + b);
+		b=(1>2);
+		System.out.println("b : "+b);
+		
+	}
+}
---
+++ booleanTest/src/module-info.java
@@ -0,0 +1,2 @@
+module booleanTest {
+}
